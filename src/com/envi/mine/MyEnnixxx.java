2466de8b1b58c7092f5690b4129ae6b62d203965
Ennix12345 <ennix123@gmail.com>
2018-09-24 12:45:11 +0300

    Changed

--- src/com/envi/mine/MyEnnixxx.java
+++ src/com/envi/mine/MyEnnixxx.java
@@ -2,6 +2,7 @@ package com.envi.mine;
 
 public class MyEnnixxx {
     public static void main(String[] args) {
-        System.out.println("Hi from Ennix!");
+        System.out.println("Hi from Enn!");
+        System.out.println("Yes!");
     }
 }
